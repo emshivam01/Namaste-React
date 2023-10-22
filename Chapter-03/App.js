729d0ba7e6759792d210b499a91112cf93029507
Shivam Sharma <emshivam01@gmail.com>
2023-10-22 05:50:57 +0530

    chapter-03 Laying the foundation

---
+++ Chapter-03/App.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const Title = () => {
+  return (
+    <div className="border-2 border-gray-700">
+      <h1 id="heading" className="text-2xl text-white">
+        Title
+      </h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Title />);
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const Title = () => {
+  return (
+    <div className="border-2 border-gray-700">
+      <h1 id="heading" className="text-2xl text-white">
+        Title
+      </h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Title />);
+>>>>>>> 904e9dc (chapter-03 laying the foundation)
